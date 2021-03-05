2522395de2d52f13d5cfed1aa6c0e88aa89713fd
Rafael <rafcasto@gmail.com>
2021-03-06 00:39:46 +1300

    docker server

---
+++ src/main/kotlin/com/microservice/chapter07/Chapter07Application.kt
@@ -0,0 +1,19 @@
+package com.microservice.chapter07
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@SpringBootApplication
+class Chapter07Application
+
+@RestController
+class GreetingsController{
+	@GetMapping("/greetings")
+	fun greetings() = "hello from Docker"
+}
+
+fun main(args: Array<String>) {
+	runApplication<Chapter07Application>(*args)
+}
---
+++ src/test/kotlin/com/microservice/chapter07/Chapter07ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.microservice.chapter07
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Chapter07ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
